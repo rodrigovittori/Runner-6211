fe47510f3bceeadbc4261e74c714accd5c64c8e9
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-15 13:01:53 -0300

    Initial commit

---
+++ CharacterScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Runner 6211
+
+public class CharacterScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed; // Velocidad de mov. de mi PJ
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+}
+// Fin de la clase CharacterScript
