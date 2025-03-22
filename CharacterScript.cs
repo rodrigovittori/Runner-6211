9155220bb1037cda7cc7eb88a271d779b303877a
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-22 12:54:03 -0300

    [M1.L4] - Actividad Nº 4
    Implementamos salto

--- CharacterScript.cs
+++ CharacterScript.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Runner 6211 - [M1.L3] - Actividad Nº 5 "¡Movimiento entre los carriles!"
+// Runner 6211 - [M1.L4] - Actividad Nº 4 "¡Vamos a configurar el salto!"
 
 public class CharacterScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed; // Velocidad de mov. de mi PJ
+    [SerializeField] private float jumpForce; // 7f
     
     void Start()
     {
@@ -26,6 +27,11 @@ public class CharacterScript : MonoBehaviour
         // Derecha
         if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && transform.position.x < 9)
             { transform.Translate(9, 0, 0); } // Mover el personaje 9 unidades a la derecha
+
+        // Condición de Salto
+        if ( Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) )
+            { rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); }
+
     }
     
 private void FixedUpdate()
